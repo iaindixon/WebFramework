1fe5ba88fd1f67cb1114b85d7a84c74a39d215d2
iaindixon <iain@dixon.net>
2020-07-13 21:25:14 -0400

    Initial Code commit, working spring boot deploy

---
+++ src/main/java/framework/server/WebServer.java
@@ -0,0 +1,14 @@
+package framework.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServer.class, args);
+    }
+
+}
